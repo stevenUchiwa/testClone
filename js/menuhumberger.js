9405641c81088462255acf9ac8a473af81ee86e8
simeone <razanajatovo.simeone@gmail.com>
2021-10-19 16:02:15 +0200

    reverse animation

--- js/menuhumberger.js
+++ js/menuhumberger.js
@@ -6,10 +6,19 @@ const centralLine = document.querySelector(".humburgerMidle");
 const contentHumburger = document.querySelector(".containerMenuHumburger");
 const elBefore = window.getComputedStyle(contentHumburger, ':before');
 
-
+var cmpAnimation = true;
 
 contentHumburger.addEventListener('click',function(evt,el){
-    centralLine.style.animation = 'menuTransform 2s ease-in-out both';
-    centralLine.style.setProperty('--animation12','menuHumburgerGauche 2s ease-in-out both');
-    centralLine.style.setProperty('--animation11','menuHumbergerDroite 2s ease-in-out both');
+    if(cmpAnimation){
+        centralLine.style.animation = 'menuTransform 2s ease-in-out both';
+        centralLine.style.setProperty('--animation12','menuHumburgerGauche 2s ease-in-out both');
+        centralLine.style.setProperty('--animation11','menuHumbergerDroite 2s ease-in-out both');
+        cmpAnimation = false;
+    }else{
+        centralLine.style.animation = 'menuTransform 2s ease-in-out reverse both';
+        centralLine.style.setProperty('--animation12','menuHumburgerGauche 2s ease-in-out reverse both');
+        centralLine.style.setProperty('--animation11','menuHumbergerDroite 2s ease-in-out reverse both');
+        cmpAnimation = true;
+    }
+    
 })
