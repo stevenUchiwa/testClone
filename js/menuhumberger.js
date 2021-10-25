05ceacab4a3a982f1aadd617b5cb1b64577b8755
simeone <razanajatovo.simeone@gmail.com>
2021-10-25 15:44:00 +0200

    fix animation arrows

--- js/menuhumberger.js
+++ js/menuhumberger.js
@@ -8,16 +8,20 @@ const elBefore = window.getComputedStyle(contentHumburger, ':before');
 
 var cmpAnimation = true;
 
+console.log('affichage en premier');
+
 contentHumburger.addEventListener('click',function(evt,el){
     if(cmpAnimation){
-        centralLine.style.animation = 'menuTransform 2s ease-in-out both';
-        centralLine.style.setProperty('--animation12','menuHumburgerGauche 2s ease-in-out both');
-        centralLine.style.setProperty('--animation11','menuHumbergerDroite 2s ease-in-out both');
+        centralLine.style.animation = 'menuTransform 1s ease-in-out normal 1 forwards';
+        centralLine.style.setProperty('--animation12','menuHumburgerGauche 1s ease-in-out normal 1 forwards');
+        centralLine.style.setProperty('--animation11','menuHumbergerDroite 1s ease-in-out normal 1 forwards');
+        console.log('cmp',true);
         cmpAnimation = false;
     }else{
-        centralLine.style.animation = 'menuTransform 2s ease-in-out reverse both';
-        centralLine.style.setProperty('--animation12','menuHumburgerGauche 2s ease-in-out reverse both');
-        centralLine.style.setProperty('--animation11','menuHumbergerDroite 2s ease-in-out reverse both');
+        centralLine.style.animation = 'menuTransformReverse 1s ease-in normal 1 forwards';
+        centralLine.style.setProperty('--animation12','menuHumburgerGaucheReverse 1s ease-in normal 1 forwards');
+        centralLine.style.setProperty('--animation11','menuHumbergerDroiteReverse 1s ease-in normal 1 forwards');
+        console.log('cmp-----------',false);
         cmpAnimation = true;
     }
     
